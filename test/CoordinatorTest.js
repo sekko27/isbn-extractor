6ee35934a041aaa7cddae32bfaf8020502af5aa5
Balazs Kovacs <balazs.kovacs@metrix.co.hu>
2018-01-15 16:24:50 +0100

    Coordinator' add method return contextual result.

--- test/CoordinatorTest.js
+++ test/CoordinatorTest.js
@@ -1,28 +1,21 @@
-const Promise = require('bluebird');
-const chai  = require('chai');
-const cap = require('chai-as-promised');
-
-chai.use(cap);
-chai.should();
+const {assert} = require('chai');
 
 const {Coordinator} = require.main.require('index');
 
 function extract(input) {
-    return Promise.fromCallback((cb) => {
-        const result = [];
-        const coordinator = new Coordinator();
-        coordinator.on('isbn', (isbn) => result.push(isbn));
-        coordinator.on('finish', () => cb(null, result));
-        for (let c of input) {
-            coordinator.add(c.charCodeAt(0));
+    let result = [];
+    const coordinator = new Coordinator();
+    for (let c of input) {
+        const r = coordinator.add(c.charCodeAt(0));
+        if (r !== undefined) {
+            result = result.concat(r);
         }
-        coordinator.emit('finish');
-    });
-
+    }
+    return result;
 }
 describe('Coordinator', function() {
     it('should extract (emit) valid ISBN-10', function() {
-        return extract('1466579986').should.become(['1466579986']);
+        assert.deepEqual(extract('1466579986'), ['1466579986']);
     });
     it('should extract (emit) multiple isbns', function() {
         const text = `Library of Congress Control Number: 2006926168
@@ -30,6 +23,6 @@ describe('Coordinator', function() {
             ISBN-10: 0-470-03746-6
             Manufactured in the United States of America
             10 9 8 7 6 5 4 3 2 1`;
-        return extract(text).should.become(['9780470037461', '0470037466']);
+        return assert.deepEqual(extract(text), ['9780470037461', '0470037466']);
     });
 });
